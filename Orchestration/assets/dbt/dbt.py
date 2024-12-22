59ea53d11419eb1c2fc60f2415561b58077f5d23
vedaBommareddy23 <veda.bommareddy@gmail.com>
2024-12-22 11:48:19 -0800

    orchestration-dagster dbt added

---
+++ Orchestration/assets/dbt/dbt.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from dagster_dbt import DbtCliResource
+from dagster import AssetExecutionContext
+from dagster_dbt import DbtCliResource, dbt_assets
+
+# Configure dbt CLI resource
+dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "..", "Transformation","polygon_dbt").resolve()
+dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
+
+# Configure manifest.json file path
+dbt_manifest_path = (
+        dbt_warehouse_resource.cli(
+            ["--quiet", "parse"],
+            target_path=Path("target"),
+        )
+        .wait()
+        .target_path.joinpath("manifest.json")
+)
+
+# Load dbt assets from manifest.json file
+@dbt_assets(manifest=dbt_manifest_path)
+def dbt_warehouse(context: AssetExecutionContext, dbt_warehouse_resource: DbtCliResource):
+    yield from dbt_warehouse_resource.cli(["build"], context=context).stream()
